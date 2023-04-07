a2add87c36918a4518415f93e99bcb2b69cc1d9a
krant <kranthireddy.mekala@gmail.com>
2023-04-07 14:40:08 +0530

    Add project files.

---
+++ windows editable gridview/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace windows_editable_gridview
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(4, 183);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(687, 316);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.ColumnHeaderMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGridView1_ColumnHeaderMouseClick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(356, 47);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "delete";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(616, 47);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "export";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(710, 183);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersWidth = 51;
+            this.dataGridView2.RowTemplate.Height = 24;
+            this.dataGridView2.Size = new System.Drawing.Size(615, 316);
+            this.dataGridView2.TabIndex = 3;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(850, 68);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(95, 20);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(952, 68);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1319, 501);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ windows editable gridview/Form1.cs
@@ -0,0 +1,239 @@
+﻿using System.Data.SqlClient;
+using System.Data;
+using System.Windows.Forms;
+using OfficeOpenXml;
+using System.IO;
+
+namespace windows_editable_gridview
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public bool IncludeHeaders { get; set; } = true;
+        public int columnIndex { get; set; } = -1;
+
+        
+        private void LoadData()
+        {
+            string connectionString = "Data Source=.;Initial Catalog=sample;Integrated Security=True;";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string query = "SELECT * FROM dbo.Employees";
+
+                SqlDataAdapter adapter = new SqlDataAdapter(query, connection);
+                DataTable data = new DataTable();
+                adapter.Fill(data);
+
+                dataGridView1.DataSource = data;
+            }
+        }
+
+        private void Form1_Load(object sender, System.EventArgs e)
+        {
+            LoadData();
+        }
+        private void DeleteColumn()
+        {
+            if (dataGridView1.SelectedCells.Count > 0)
+            {
+                int columnIndex = dataGridView1.SelectedCells[0].ColumnIndex;
+                dataGridView1.Columns.RemoveAt(columnIndex);
+            }
+        }
+
+        private void button1_Click(object sender, System.EventArgs e)
+        {
+            DeleteColumn();
+        }
+
+        private void ExportToExcel()
+        {
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+            // Create a new Excel package
+            // Create a new Excel package
+
+            using (ExcelPackage excelPackage = new ExcelPackage())
+            {
+                // Add a new worksheet
+                ExcelWorksheet worksheet = excelPackage.Workbook.Worksheets.Add("Sheet1");
+
+                // Set the column headers if IncludeHeaders option is enabled
+                if (IncludeHeaders)
+                {
+                    for (int i = 0; i < dataGridView1.Columns.Count; i++)
+                    {
+                        worksheet.Cells[1, i + 1].Value = dataGridView1.Columns[i].HeaderText;
+                    }
+                }
+
+                // Fill the data
+                for (int i = 0; i < dataGridView1.Rows.Count; i++)
+                {
+                    for (int j = 0; j < dataGridView1.Columns.Count; j++)
+                    {
+                        worksheet.Cells[i + 2, j + 1].Value = dataGridView1.Rows[i].Cells[j].Value;
+                    }
+                }
+
+                // Save the Excel file
+                FileInfo excelFile = new FileInfo("output.xlsx");
+                excelPackage.SaveAs(excelFile);
+            }
+
+
+        }
+
+        private void button2_Click(object sender, System.EventArgs e)
+
+        {
+            dataGridView2.DataSource = null;
+            foreach (DataGridViewColumn column in dataGridView1.Columns)
+            {
+                if (column.Index != columnIndex)
+                {
+                    dataGridView2.Columns.Add(column.Clone() as DataGridViewColumn);
+                }
+
+            }
+            for (int i = 0; i < dataGridView1.Rows.Count; i++)
+            {
+                // Create a new row in the DataGridView
+                DataGridViewRow row = new DataGridViewRow();
+
+                // Copy the cell values from the first DataGridView to the new one, except for the removed column
+                
+                for (int j = 0; j < dataGridView1.Columns.Count; j++)
+                {
+                    if (j != columnIndex)
+                    {
+                        row.Cells.Add(new DataGridViewTextBoxCell { Value = dataGridView1.Rows[i].Cells[j].Value });
+                    }
+                }
+
+                // Add the new row to the new DataGridView
+                dataGridView2.Rows.Add(row);
+                
+            }
+            //dataGridView2.DataSource = dataGridView1;
+        }
+        private void dataGridView1_ColumnHeaderMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            // Get the clicked column index
+            int columnIndex = e.ColumnIndex;
+
+            // Set the Selected property of all the cells in the clicked column to true
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                DataGridViewCell cell = row.Cells[columnIndex];
+                cell.Selected = true;
+            }
+        }
+        private void checkBox1_CheckedChanged(object sender, System.EventArgs e)
+        {
+            IncludeHeaders = false;
+            
+        }
+
+       
+        // private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        //    {
+        //        // Check if the click is on a header cell
+        //        if (e.RowIndex == -1)
+        //        {
+        //            // Get the index of the clicked column
+        //            int columnIndex = e.ColumnIndex;
+
+        //            // Remove the selected column from the DataGridView
+        //            dataGridView1.Columns.RemoveAt(columnIndex);
+
+        //            // Clone the DataGridView to create a new instance with the updated data
+        //            DataGridView dataGridView2 = (DataGridView)dataGridView1.Clone();
+
+        //            // Populate the new DataGridView with the updated data
+        //            for (int i = 0; i < dataGridView1.Rows.Count; i++)
+        //            {
+        //                // Create a new row in the DataGridView
+        //                DataGridViewRow row = new DataGridViewRow();
+
+        //                // Copy the cell values from the first DataGridView to the new one, except for the removed column
+        //                for (int j = 0; j < dataGridView1.Columns.Count; j++)
+        //                {
+        //                    if (j != columnIndex)
+        //                    {
+        //                        row.Cells.Add(new DataGridViewTextBoxCell { Value = dataGridView1.Rows[i].Cells[j].Value });
+        //                    }
+        //                }
+
+        //                // Add the new row to the new DataGridView
+        //                dataGridView2.Rows.Add(row);
+        //            }
+
+        //            // Display the new DataGridView
+        //            dataGridView2.Dock = DockStyle.Fill;
+        //            panel2.Controls.Add(dataGridView2);
+        //        }
+        //    }
+
+        //}
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e, Form1 form1)
+        {
+            // Check if the click is on a header cell
+            if (e.RowIndex == -1)
+            {
+                // Get the index of the clicked column
+                int columnIndex = e.ColumnIndex;
+
+                // Remove the selected column from the DataGridView
+                dataGridView1.Columns.RemoveAt(columnIndex);
+
+                // Create a new DataGridView control
+                //DataGridView dataGridView2 = new DataGridView();
+
+                //// Set the properties of the new DataGridView control to match those of the original DataGridView control
+                //dataGridView2.AllowUserToAddRows = false;
+                //dataGridView2.AllowUserToDeleteRows = false;
+                //dataGridView2.AllowUserToResizeRows = false;
+                //dataGridView2.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+                //dataGridView2.Dock = DockStyle.Fill;
+                //dataGridView2.ReadOnly = true;
+                //dataGridView2.RowHeadersVisible = false;
+                //form1.Controls.Add(dataGridView2);
+
+                // Populate the new DataGridView control with the updated data
+                //for (int i = 0; i < dataGridView1.Rows.Count; i++)
+                //{
+                //    // Create a new row in the DataGridView
+                //    DataGridViewRow row = new DataGridViewRow();
+
+                //    // Copy the cell values from the first DataGridView to the new one, except for the removed column
+                //    for (int j = 0; j < dataGridView1.Columns.Count; j++)
+                //    {
+                //        if (j != columnIndex)
+                //        {
+                //            row.Cells.Add(new DataGridViewTextBoxCell { Value = dataGridView1.Rows[i].Cells[j].Value });
+                //        }
+                //    }
+
+                //    // Add the new row to the new DataGridView
+                //    dataGridView2.Rows.Add(row);
+                //}
+            }
+        }
+
+        private void button3_Click(object sender, System.EventArgs e)
+        {
+           
+                dataGridView1.ColumnHeadersVisible = false;
+            
+
+        }
+    }
+}
+
